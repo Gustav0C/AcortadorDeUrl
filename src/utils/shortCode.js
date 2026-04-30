73c9880498a9a963558639e8b142af5a4200d4f1
Gustav0C <gcanales58@gmail.com>
2026-04-30 01:56:49 -0500

    fix: remove nanoid ESM dep, use only shortid for CJS compatibility

--- src/utils/shortCode.js
+++ src/utils/shortCode.js
@@ -1,19 +1,8 @@
-const { customAlphabet } = require('nanoid');
 const shortid = require('shortid');
 
-function createShortCodeGenerator({
-  nanoidFactory = customAlphabet,
-  shortidFactory = shortid.generate,
-} = {}) {
-  try {
-    const nanoid = nanoidFactory(
-      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-      10
-    );
-    return () => nanoid();
-  } catch (e) {
-    return () => shortidFactory();
-  }
+function createShortCodeGenerator() {
+  return () => shortid.generate();
+}
 }
 
 function isValidShortCode(value) {
