7199dd3cd3db196429ad07c6bd409e760a1ed574
Gustav0C <gcanales58@gmail.com>
2026-04-30 02:00:50 -0500

    fix: syntax error in shortCode generator

--- src/utils/shortCode.js
+++ src/utils/shortCode.js
@@ -3,7 +3,6 @@ const shortid = require('shortid');
 function createShortCodeGenerator() {
   return () => shortid.generate();
 }
-}
 
 function isValidShortCode(value) {
   return /^[a-zA-Z0-9]+$/.test(value) && value.length <= 20;
